bafc1520699f5ab2fb5e00844dd107f9121a2a33
Jiozang theophane <jiozangtheophane@gmail.com>
2023-08-25 19:03:42 +0200

    merge

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import "@testing-library/jest-dom";
@@ -10,19 +10,18 @@ import { ROUTES } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store.js"
 import userEvent from "@testing-library/user-event";
-
+// jest.mock("../app/Store", () => mockStore)
 describe("Given I am connected as an employee", () => {
   let newBill;
   let mockOnNavigate;
-  let bill;
   beforeAll(() => {
     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "test@test.com" }));
     const html = NewBillUI()
-    // mockOnNavigate = jest.fn();
+    // // mockOnNavigate = jest.fn();
     mockOnNavigate = (pathname) => { document.body.innerHTML = ROUTES({ pathname }) }
     document.body.innerHTML = html
-    // Instanciez NewBill avec les mocks
+
     newBill = new NewBill({ document: document, onNavigate: mockOnNavigate, store: mockStore, localStorage: window.localStorage });
   })
 
@@ -53,57 +52,106 @@ describe("Given I am connected as an employee", () => {
 
   describe('handleChangeFile', () => {
     // Test pour la fonction handleChangeFile
+    // const newBill = new NewBill({ document: document, onNavigate: mockOnNavigate, store: mockStore, localStorage: window.localStorage });
     test('should handle file change', () => {
-       const fileChangeEvent = {
+      const fileChangeEvent = {
         preventDefault: jest.fn(),
         target: { value: "https://localhost:3456/images/test.jpg" },
       };
       const file = screen.getByTestId("file");
       const handleChangeFile1 = jest.fn((e) => newBill.handleChangeFile(fileChangeEvent))
-      const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => {}); // création un espion alertSpy pour surveiller les appels à la fonction window.alert
+      const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { }); // création un espion alertSpy pour surveiller les appels à la fonction window.alert
       file.addEventListener("change", handleChangeFile1)
       userEvent.upload(file, 'theo.png')
       expect(alertSpy).toHaveBeenCalledWith(
         "Invalid file extension. Only jpg, jpeg, and png files are allowed."
       );
-      
     });
   });
+});
 
-  describe('handleSubmit', () => {
-    // Test pour la fonction handleSubmit
-    test('should handle form submit', () => {
-      const submitEvent = {
-        preventDefault: jest.fn(),
-        target: {
-          querySelector: jest.fn().mockImplementation((selector) => {
-            switch (selector) {
-              case 'input[data-testid="datepicker"]':
-                return { value: "2021-12-01" };
-              case 'select[data-testid="expense-type"]':
-                return { value: "restaurant" };
-              case 'input[data-testid="expense-name"]':
-                return { value: "Test Restaurant" };
-              case 'input[data-testid="amount"]':
-                return { value: "100" };
-              case 'input[data-testid="vat"]':
-                return { value: "20" };
-              case 'input[data-testid="pct"]':
-                return { value: "10" };
-              case 'textarea[data-testid="commentary"]':
-                return { value: "Test commentary" };
-              default:
-                return null;
-            }
-          }),
-        },
-      };
+describe('handleSubmit', () => {
+  let newBill;
+  let mockOnNavigate;
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "test@test.com" }));
+    // const html = NewBillUI()
+    // // mockOnNavigate = jest.fn();
+    mockOnNavigate = (pathname) => { document.body.innerHTML = ROUTES({ pathname }) }
+    document.body.innerHTML = NewBillUI()
+
+    newBill = new NewBill({ document: document, onNavigate: mockOnNavigate, store: mockStore, localStorage: window.localStorage });
+  })
+  // Tester le formulaire avec les champs bien remplis
+  describe('should handle form submit', () => {
+    test('with the correct values', async () => {
+      jest.spyOn(mockStore, 'bills');
+      newBill.updateBill = jest.fn();
+
+      const bill = {
+        email: undefined,
+        type: "Transports",
+        name: "Vol Paris Londres",
+        date: "2022-01-01",
+        amount: 348,
+        vat: "70",
+        pct: 20,
+        commentary: "Test commentary",
+        fileUrl: "./theophane.png",
+        fileName: "theophane.png",
+        status: "pending",
+      }
+
+      const formElement = screen.getByTestId("form-new-bill");
+      const expenseType = screen.getByTestId("expense-type");
+      const expenseName = screen.getByTestId("expense-name");
+      const datePicker = screen.getByTestId("datepicker");
+      const amount = screen.getByTestId("amount");
+      const vat = screen.getByTestId("vat");
+      const pct = screen.getByTestId("pct");
+      const commentary = screen.getByTestId("commentary");
+
+      fireEvent.change(expenseType, { target: { value: bill.type } });
+      fireEvent.change(expenseName, { target: { value: bill.name } });
+      fireEvent.change(datePicker, { target: { value: bill.date } });
+      fireEvent.change(amount, { target: { value: bill.amount } });
+      fireEvent.change(vat, { target: { value: bill.vat } });
+      fireEvent.change(pct, { target: { value: bill.pct } });
+      fireEvent.change(commentary, { target: { value: bill.commentary } });
+      newBill.fileName = bill.fileName;
+      newBill.fileUrl = bill.fileUrl;
+
       const submit = screen.getByTestId("submit");
-      const submitEvent1 = jest.fn((e) => newBill.handleSubmit(submitEvent))
-      submit.addEventListener("click", submitEvent1)
+      const submitEvent1 = jest.fn((e) => newBill.handleSubmit(e))
+      const mockStoreBills = mockStore.bills();
+      formElement.addEventListener("submit", submitEvent1)
       userEvent.click(submit)
-      expect(submitEvent1).toHaveBeenCalled()
-      expect(submitEvent1).toBeTruthy();
+
+      expect(submitEvent1).toHaveBeenCalled();
+      expect(newBill.updateBill).toHaveBeenCalled();
+      expect(newBill.updateBill).toHaveBeenCalledWith(bill);
+      expect(mockStoreBills).toBeTruthy();
+
+      expect(screen.getByText('Mes notes de frais')).toBeTruthy();
     });
+
+  })
+  // Tester la fonction handleSubmit avec les champs partiellement remplis
+  describe('Test the handleSubmit function', () => {
+    test(' handleSubmit function failure', async () => {
+      document.body.innerHTML = '';
+      document.body.innerHTML = NewBillUI();
+      newBill.updateBill = jest.fn();
+
+      const formElement = screen.getByTestId("form-new-bill");
+
+      fireEvent.submit(formElement)
+      // Assert
+      expect(newBill.updateBill).not.toHaveBeenCalled();
+      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy();
+
+    });
+
   });
-})
+});
